3e41e0a583159b078005c99ba4f277608d6e8adc
andykuo8766 <andykuo8766@gapp.nthu.edu.tw>
2021-12-09 04:22:26 -0800

    Update shiny.c

--- shiny.c
+++ shiny.c
@@ -11,10 +11,9 @@ int main(){
 	for(int j = 0 ; j < times; j++){
 		result = result * 0.99;	
 	}
+	printf("No Shiny Probability is : %f%%\n",(result)*100);
 
-	printf("Not Shiny Probability is : %f%\n",(result)*100);
-
-	printf("Shiny Probability is : %f%\n",(1-result)*100);
+	printf("At least one Shiny Probability is : %f%%\n",(1-result)*100);
 
 	return 0;
 }
