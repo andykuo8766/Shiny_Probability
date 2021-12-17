71fdfd9e588ee9fe7f6186be44f2ba52019f73b5
andykuo8766 <andykuo8766@gapp.nthu.edu.tw>
2021-12-17 03:04:15 -0800

    Upload go.c

--- Makefile
+++ Makefile
@@ -1,15 +1,20 @@
 
 CC:=g++
-exe:=shiny
-obj:=shiny.o
+exe:=shiny go
+obj:=shiny.o go.o
 
-all:$(obj)
-	$(CC) -o $(exe) $(obj)  
 %.o:%.c
 	$(CC) -c $^ -o $@
+
+all:$(obj)
+	$(CC) -o shiny shiny.o 
+	$(CC) -o go go.o 
 
 clean:
 	rm -rf $(obj) $(exe)
 
-run:
-	./$(exe)
+runshiny:
+	./shiny
+
+rungo:
+	./go
---
+++ go.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int times = 0;
+	double result = 1;
+	printf("Please Input Times : ");
+	scanf("%d",&times);
+	
+	for(int j = 0 ; j < times; j++){
+		result = (result * ((double)23/24));	
+	}
+	printf("No Shiny Probability is : %f%%\n",(result)*100);
+
+	printf("At least one Shiny Probability is : %f%%\n",(1-result)*100);
+
+	return 0;
+}
